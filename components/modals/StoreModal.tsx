36e476fc8a6b594fca1d793a2ff136cc77ef5488
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-08-13 19:47:55 +0200

    feat: add form to create store modal

--- components/modals/StoreModal.tsx
+++ components/modals/StoreModal.tsx
@@ -2,9 +2,38 @@
 
 import { useStoreModal } from '@/hooks/useStoreModal';
 import Modal from '../ui/modal';
+import * as z from 'zod';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '../ui/form';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+
+const formSchema = z.object({
+  name: z.string().min(1),
+});
 
 const StoreModal = () => {
   const store = useStoreModal();
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      name: '',
+    },
+  });
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    // TODO: Create store
+    console.log(values);
+  };
 
   return (
     <Modal
@@ -13,7 +42,33 @@ const StoreModal = () => {
       isOpen={store.isOpen}
       onClose={store.onClose}
     >
-      Future Create Store Modal
+      <section>
+        <div className="space-y-4 py-2 pb-4">
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="E-commerce" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <div className="pt-6 space-x-2 flex items-center justify-end w-full">
+                <Button variant="outline" onClick={store.onClose}>
+                  Cancel
+                </Button>
+                <Button type="submit">Continue</Button>
+              </div>
+            </form>
+          </Form>
+        </div>
+      </section>
     </Modal>
   );
 };
