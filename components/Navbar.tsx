73c9bdc237743c1fbef0b775ace39260b3de75cf
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-08-13 23:01:30 +0200

    feat: add store switcher to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,22 @@
-import { UserButton } from '@clerk/nextjs';
+import { UserButton, auth } from '@clerk/nextjs';
 import MainNav from './MainNav';
+import StoreSwitcher from './StoreSwitcher';
+import { redirect } from 'next/navigation';
+import prismadb from '@/lib/prismadb';
 
-const Navbar = () => {
+const Navbar = async () => {
+  const { userId } = auth();
+
+  if (!userId) {
+    redirect('/sign-in');
+  }
+
+  const stores = await prismadb.store.findMany({ where: { userId } });
+
   return (
     <div className="border-b">
       <div className="h-16 flex items-center px-4">
-        <div>Store Switcher</div>
+        <StoreSwitcher items={stores} />
         <MainNav className="mx-6" />
         <div className="ml-auto flex items-center space-x-4">
           <UserButton afterSignOutUrl="/" />
--- types.d.ts
+++ types.d.ts
@@ -1,11 +1,13 @@
+import type { Store } from '@prisma/client';
+
 export interface CreateStoreAPIResponse {
   data: Store;
 }
 
-export interface Store {
+/* export interface Store {
   id: string;
   name: string;
   userId: string;
   createdAt: Date;
   updatedAta: Date;
-}
+} */
