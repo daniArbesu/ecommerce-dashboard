ef59da7c1823e437b43e29f65eb606fe45e44737
daniArbesu <daniel.arbesu.sanchez@gmail.com>
2023-08-13 21:10:41 +0200

    feat: add api to post new store into db

--- .eslintrc.js
+++ .eslintrc.js
@@ -30,6 +30,8 @@ module.exports = {
   rules: {
     '@typescript-eslint/explicit-function-return-type': 'off',
     'react/react-in-jsx-scope': 'off',
-    'react/prop-types': 'off'
+    'react/prop-types': 'off',
+    '@typescript-eslint/strict-boolean-expressions': 'off',
+    '@typescript-eslint/no-misused-promises': 'off'
   }
 };
